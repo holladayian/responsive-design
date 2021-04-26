2647ba9d4d6d66b22bc4fa9a8db6c8224c486aa8
Ian Holladay <holladayian@gmail.com>
2021-04-26 04:47:27 -0600

    Remove carousel :(

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const leftButton = document.querySelector(".ab-left");
-const rightButton = document.querySelector(".ab-right");
-const currentImg = document.querySelector(".current-image");
-const dot0 = document.querySelector(".dot-0");
-const dot1 = document.querySelector(".dot-1");
-const dot2 = document.querySelector(".dot-2");
+// const leftButton = document.querySelector(".ab-left");
+// const rightButton = document.querySelector(".ab-right");
+// const currentImg = document.querySelector(".current-image");
+// const dot0 = document.querySelector(".dot-0");
+// const dot1 = document.querySelector(".dot-1");
+// const dot2 = document.querySelector(".dot-2");
 const homeButton = document.querySelector(".logo-container");
 const aboutButton = document.querySelector(".about");
 const projectsButton = document.querySelector(".projects");
@@ -13,50 +13,50 @@ const aboutView = document.querySelector(".about-view");
 const projectsView = document.querySelector(".projects-view");
 const contactView = document.querySelector(".contact-view");
 
-let imgIterator = 0;
-let imgIndex = [
-  './img/headshot.jpg',
-  './img/london.jpeg',
-  './img/headshot.jpg'
-]
+// let imgIterator = 0;
+// let imgIndex = [
+//   './img/headshot.jpg',
+//   './img/london.jpeg',
+//   './img/headshot.jpg'
+// ]
 
-leftButton.addEventListener("click", cycleLeft)
-rightButton.addEventListener("click", cycleRight)
+// leftButton.addEventListener("click", cycleLeft)
+// rightButton.addEventListener("click", cycleRight)
 aboutButton.addEventListener("click", viewAbout)
 projectsButton.addEventListener("click", viewProjects)
 contactButton.addEventListener("click", viewContacts)
 homeButton.addEventListener("click", viewHome)
 
-function cycleLeft() {
-  if (imgIterator > 0) {
-    imgIterator --;
-    currentImg.src = imgIndex[imgIterator];
-    dotChanger();
-  }
-}
+// function cycleLeft() {
+//   if (imgIterator > 0) {
+//     imgIterator --;
+//     currentImg.src = imgIndex[imgIterator];
+//     dotChanger();
+//   }
+// }
 
-function cycleRight() {
-  if (imgIterator < 2) {
-    imgIterator ++;
-    currentImg.src = imgIndex[imgIterator]
-    dotChanger();
-  }
-}
+// function cycleRight() {
+//   if (imgIterator < 2) {
+//     imgIterator ++;
+//     currentImg.src = imgIndex[imgIterator]
+//     dotChanger();
+//   }
+// }
 
-function dotChanger() {
-  dot0.src = "./img/dot-full.svg";
-  dot1.src = "./img/dot-full.svg";
-  dot2.src = "./img/dot-full.svg";
-  if (!imgIterator) {
-    dot0.src = "./img/dot.svg";
-  }
-  else if (imgIterator === 1) {
-    dot1.src = "./img/dot.svg";
-  }
-  else {
-    dot2.src = "./img/dot.svg";
-  }
-}
+// function dotChanger() {
+//   dot0.src = "./img/dot-full.svg";
+//   dot1.src = "./img/dot-full.svg";
+//   dot2.src = "./img/dot-full.svg";
+//   if (!imgIterator) {
+//     dot0.src = "./img/dot.svg";
+//   }
+//   else if (imgIterator === 1) {
+//     dot1.src = "./img/dot.svg";
+//   }
+//   else {
+//     dot2.src = "./img/dot.svg";
+//   }
+// }
 
 function viewAbout() {
   landingView.classList.add("hidden");
